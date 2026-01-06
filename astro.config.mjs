04d7209327fc9d09af1f1ba1389580fc9eaac461
idolsdiagram <idolsdiagram@gmail.com>
2026-01-06 10:24:53 +0900

    update: sitemap生成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { config } from 'dotenv';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
+config();
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: process.env.SITE_URL,
+  integrations: [react(), sitemap()],
   vite: {
     ssr: {
       noExternal: ['beercss', 'material-dynamic-colors']
